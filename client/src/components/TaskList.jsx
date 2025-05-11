8860ab3104d3bc00eca46b9b51154242297d2935
Gabriel <ar.gabrielss@gmail.com>
2025-05-10 22:23:17 -0300

    Front: add search bar

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
 
-export default function TaskList({ onTaskDeleted, onEdit }) {
+export default function TaskList({ onTaskDeleted, onEdit, reloadTrigger }) {
     const [tasks, setTasks] = useState([]);
+    const [searchTerm, setSearch] = useState('');
   
     const fetchTasks = async () => {
       try {
-        const response = await api.get('/');
+        const response = await api.get('/', {
+          params: { searchTerm }
+        });
         setTasks(response.data);
       } catch (err) {
         console.error("Erro ao carregar tarefas: ", err);
@@ -24,11 +27,19 @@ export default function TaskList({ onTaskDeleted, onEdit }) {
   
     useEffect(() => {
       fetchTasks();
-    }, []);
+    }, [reloadTrigger, searchTerm]);
   
     return (
       <div className="bg-white p-6 rounded-2xl shadow-md max-w-xl mb-6 space-y-4">
         <h2 className="text-2xl font-bold text-gray-700">Minhas Tarefas</h2>
+        <input
+            type="text"
+            name="search"
+            placeholder="Buscar por palavra-chave..."
+            value={searchTerm}
+            onChange={ (e) => {setSearch(e.target.value)} }
+            className="w-full p-2 border border-gray-300 rounded mb-2"
+        />
         <ul className="text-gray-600 space-y-2">
           {tasks.map((task) => (
             <li key={task.id} className="bg-gray-100 p-4 rounded flex justify-between items-center">
