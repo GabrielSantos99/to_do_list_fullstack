0f20c9fb1c396ff1e6929e4bb351cedbf7d26b61
Gabriel <ar.gabrielss@gmail.com>
2025-07-25 09:40:05 -0300

    Apply mobile-first concept and adjust the spacing of elements

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
     return(
         <div className="">
-            <header className="fixed w-full top-0 left-0 bg-slate-100 shadow p-5">
+            <header className="bg-slate-100 shadow p-5">
                 <div className="max-w-7xl mx-auto flex justify-between items-center">
                     <p className="text-gray-950 text-xl font-semibold px-2">To-Do List</p>
                     <nav className="px-2 justify-between">
@@ -18,7 +18,7 @@ function Home() {
                     </nav>
                 </div>
             </header>
-            <main>
+            <main className="p-8 lg:p-36">
                 <h1 className="font-bold text-center py-1">
                     Organize suas tarefas com facilidade. Faça login ou crie uma conta para começar.
                 </h1>
@@ -30,7 +30,7 @@ function Home() {
                     className="my-2 bg-blue-700 hover:bg-blue-800 text-white p-3 rounded"
                 >Faça parte do time</Link>
             </main>
-            <footer className="fixed w-full bottom-0 left-0 p-2">
+            <footer className="p-2 lg:p-5">
                 <a href="https://github.com/GabrielSantos99">Git Hub</a>
             </footer>
         </div>
