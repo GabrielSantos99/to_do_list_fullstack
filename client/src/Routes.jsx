c0279474be2a60c92fa0921709bd2fa6b4cdc1e9
Gabriel <ar.gabrielss@gmail.com>
2025-07-28 06:57:17 -0300

    Add a route to create a new task in a modal

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,22 +1,42 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
-import TaskList from "./components/TaskList";
+import TaskList from "./pages/TaskList/TaskList";
 import PrivateRoute from "./components/PrivateRoute";
+import TaskForm from "./components/TaskForm";
 
 function MainRoutes() {
+    const location = useLocation();
+    const backgroundLocation = location.state?.backgroundLocation;
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login" element={<LoginForm />}/>
-            <Route path="/register" element={<RegisterForm />}/>
-            <Route path="/tasks" element={
-                <PrivateRoute>
+        <>
+            <Routes location={ backgroundLocation || location }>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegisterForm />} />
+                <Route path="/tasks" element={
                     <TaskList />
-                </PrivateRoute>
-                }/>
-        </Routes>
+                } />
+                <Route path="/tasks/new" element={<TaskForm />} />
+            </Routes >
+
+            {backgroundLocation && (
+                <Routes>
+                    <Route
+                        path="/tasks/new"
+                        element={
+                            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                                <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
+                                    <TaskForm />
+                                </div>
+                            </div>
+                        }
+                    />
+                </Routes>
+            )}
+        </>
     );
 }
 
